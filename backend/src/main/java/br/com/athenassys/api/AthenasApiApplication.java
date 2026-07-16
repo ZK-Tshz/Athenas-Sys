4e2329b17efe95156a87e9011d338bf7fce4af88
ZK-Tshz <murilo.bap.oliv@gmail.com>
2026-07-16 08:53:01 -0300

    chore: criar estrutura inicial do projeto

---
+++ backend/src/main/java/br/com/athenassys/api/AthenasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.athenassys.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenasApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/athenassys/api/AthenasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.athenassys.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
